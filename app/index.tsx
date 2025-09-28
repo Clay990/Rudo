a26979f1080b3b5627a4c3debbef18d29470f7b2
Clay990 <asuraj991m@gmail.com>
2025-09-28 19:48:29 +0530

    Created welcome to home navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,5 @@
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import "./global.css"
-import { Heading } from '@/components/ui/heading';
-import { Text } from '@/components/ui/text';
-import { View, Image, Button, Alert, TouchableOpacity } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function App() {
-  const handlestart = () => {
-    Alert.alert("Hello");
-  }
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView className="flex-1">
-        <View className="flex-1">
-          <Image
-            style={{ width: '100%', height: '100%', borderBottomLeftRadius: 32, borderBottomRightRadius: 32 }}
-            source={require('@/assets/images/welcome.png')}
-            alt="image"
-          />
-        </View >
-        <View className="flex-1 flex-col justify-center m-5">
-          <Heading size='3xl'>Take Notes in this</Heading>
-          <Heading className="pt-4" size='3xl'>New modern Rudo app</Heading>
-          <Text className="pt-5" size='lg'>Do your note taking anywhere anytime</Text>
-            
-          <TouchableOpacity onPress={handlestart}>
-            <View className="overflow-hidden mt-10 rounded-xl bg-pink-400 justify-center items-center h-[50]">
-              <Text className="text-white font-bold text-xl">Get Started</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+export default function Page() {
+  return <Redirect href="/welcome" />;
 }
---
+++ app/welcome.tsx
@@ -0,0 +1,37 @@
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import "./global.css"
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
+import { View, Image, Button, Alert, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Welcome() {
+  const router = useRouter();
+  const handleStart = () => {
+    router.push('/home');
+  }
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1">
+        <View className="flex-1">
+          <Image
+            style={{ width: '100%', height: '100%', borderBottomLeftRadius: 32, borderBottomRightRadius: 32 }}
+            source={require('@/assets/images/welcome.png')}
+            alt="image"
+          />
+        </View >
+        <View className="flex-1 flex-col justify-center m-5">
+          <Heading size='3xl'>Take Notes in this</Heading>
+          <Heading className="pt-4" size='3xl'>New modern Rudo app</Heading>
+          <Text className="pt-5" size='lg'>Do your note taking anywhere anytime</Text>
+            
+          <TouchableOpacity onPress={handleStart}>
+            <View className="overflow-hidden mt-10 rounded-xl bg-pink-400 justify-center items-center h-[50]">
+              <Text className="text-white font-bold text-xl">Get Started</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
